123ac855a757e61ab8ca599fa3162539ad73fe01
pozy <masikapolycarp@gmail.com>
2015-09-19 13:39:06 +0300

    users controller edit

--- controllers/users.php
+++ controllers/users.php
@@ -1,21 +1,52 @@
 <?php
 // class to hold user login infomation
-class users {
-	var $user_name;
-	var $email;
-
+class Users {
+	private $b;
+	public function __construct(PDO $db) {
+		$this->db = $db;
+	}
 	public function login($username, $password) {
-		$this->username = $username;
-		$this->password = $password;
+			try {
+				$sql = "SELECT username,password FROM users WHERE username=:username AND password=:password LIMIT 1";
+				$conn = $this->db->prepare($sql);
+				$conn->execute(array(
+					':username' => $username,
+					':password' => hash('SHA256',$password)
+				));
+
+				$results = $conn->fetchColumn();
+				if($results) {
+					return true;
+				}
+			} catch(PDOException $e) {
+				return $e->getMessage();
+			}
+			return false;
 	}
 
 	public function register($user_name, $passwd) {
-		$this->username = $username;
-		$this->email    = $email;
-		$this->password = $password;
+			try {
+				$sql ="INSERT INTO users(username,password) VALUES(:username,:password)";
+				$conn = $this->db->prepare($sql);
+				$results = $conn->execute(array(
+					':username' => $user_name,
+					':password' => hash('SHA256',$passwd)
+				));
+
+				if($results) {
+					return true;
+				}
+			} catch(PDOException $e) {
+				return $e->getMessage();
+			}
+			return false;
 	}
+
 }
 
-// object instantiation
-
+/**
+include_once('../config.php');
+$usr = new Users($db);
+print_r($usr->login('pozy','password'));
+**/
 ?>
